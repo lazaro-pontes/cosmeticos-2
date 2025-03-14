9953352dbe4b76b918871042c5932faf615c62b1
João lazaro <laziruspoints@gmail.com>
2025-03-14 13:32:05 -0300

    modelo de usuário testado

---
+++ src/models/testUser.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const User = require('./User');
+const { mongoUri } = require('../config/config')
+
+mongoose.connect(mongoUri);
+
+async function test() {
+  const user = new User({
+    name: 'João',
+    email: 'joao@email.com',
+    password: 'senha123',
+  });
+  await user.save();
+  console.log('Usuário salvo com sucesso:', user);
+  mongoose.connection.close();
+}
+
+test();
